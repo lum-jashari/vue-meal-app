b5fc07c83732ae42a388d25a51fbf493fb45deb1
lum-jashari <lumijashari@hotmail.com>
2024-02-23 22:35:48 +0100

    fixed desc truncation and styling

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -13,7 +13,7 @@
         <div class="p-3">
             <h3 class="font-bold">{{ meal.strMeal }}</h3>
             <p class="mb-4">
-                {{ $filters.truncateWords(meal.strInstructions, 20) }}
+                {{ $filters.truncateWords(meal.strInstructions, 20) + "..." }}
             </p>
             <div class="flex items-center justify-between">
                 <YouTubeButton :href="meal.strYoutube" />
