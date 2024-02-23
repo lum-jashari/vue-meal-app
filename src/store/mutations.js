572b8bfb5080984b948630d64c1d12db75c6f988
lum-jashari <lumijashari@hotmail.com>
2024-02-23 22:14:42 +0100

    fixed by ingredient meal opening

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,13 +1,13 @@
 export function setSearchedMeals(state, meals) {
-    console.log("inside mutations1");
+    console.log("inside mutations2");
     state.searchedMeals = meals || [];
 }
 export function setMealsByLetter(state, meals) {
-    console.log("inside mutations1");
+    console.log("inside mutations2");
     state.mealsByLetter = meals || [];
 }
 export function setMealsByIngredients(state, meals) {
-    console.log("inside mutations1");
+    console.log("inside mutations2");
     state.mealsByIngredient = meals || [];
 }
 export function setIngredient(state, ingredient) {
--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,32 +1,37 @@
 <template>
-    <div class="p-8">
+    <div class="p-8 pb-0">
         <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+    </div>
+    <div class="px-8">
         <input
             type="text"
-            name="meals"
             v-model="keyword"
-            class="rounded border-2 bg-white border-gray-200 w-full mb-3"
-            placeholder="search for ingredients"
+            class="rounded border-2 bg-white border-gray-200 w-full mb-3 focus:ring-orange-500 focus:border-orange-500"
+            placeholder="Search for ingredients"
         />
-        <router-link
-            :to="{
-                name: 'byIngredient',
-                params: { ingredient: ingredient.strIngredient },
-            }"
-            v-for="ingredient of computedIngredients"
-            :key="ingredient.id"
-            class="block bg-white rounded p-3 mb-3 shadow"
-        >
-            <h3 class="text-2xl font-bold mb-2">
-                {{ ingredient.strIngredient }}
-            </h3>
-        </router-link>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+            <a
+                href="#"
+                @click="openIngredient(ingredient)"
+                v-for="ingredient of computedIngredients"
+                :key="ingredient.idIngredient"
+                class="block bg-white rounded p-3 mb-3 shadow"
+            >
+                <h3 class="text-2xl font-bold mb-2">
+                    {{ ingredient.strIngredient }}
+                </h3>
+            </a>
+        </div>
     </div>
 </template>
 <script setup>
 import { computed, onMounted, ref } from "vue";
 import axiosClient from "../axiosClient";
+import { useRouter } from "vue-router";
+import store from "../store";
 
+const router = useRouter();
 const keyword = ref("");
 const ingredients = ref([]);
 const computedIngredients = computed(() => {
@@ -37,6 +42,14 @@ const computedIngredients = computed(() => {
             .includes(keyword.value.toLowerCase());
     });
 });
+
+function openIngredient(ingredient) {
+    store.commit("setIngredient", ingredient);
+    router.push({
+        name: "byIngredient",
+        params: { ingredient: ingredient.strIngredient },
+    });
+}
 
 onMounted(() => {
     axiosClient.get("list.php?i=list").then(({ data }) => {
